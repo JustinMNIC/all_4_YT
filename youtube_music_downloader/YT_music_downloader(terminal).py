53dd26f588b874f3da0da0fa213709cb79a7836a
JustinMNIC <104854769+JustinMNIC@users.noreply.github.com>
2024-02-12 02:10:51 +0300

    finally found the error ....

--- youtube_music_downloader/YT_music_downloader(terminal).py
+++ youtube_music_downloader/YT_music_downloader(terminal).py
@@ -2,7 +2,7 @@ from pytube import YouTube, Playlist
 
 def download_video(url, output_path="."):
     try:
-        yt = YouTube(url, allow_oauth_cache=True, use_oauth=True) ## https://github.com/pytube/pytube/issues/1712 (for context) for some reason there is an error with the age restriction, and the only way (that I could find till now) to bypass the age restriction error is by going to the dir where your interpretor is installed (inluding the pytube lib.), open " innertube.py " and in  line 223 change ANDROID_MUSIC to something like WEB, WEB_CREATOR, and MWEB ( but this reduces the downloading speed to 0.7 mb ) :C   
+        yt = YouTube(url, allow_oauth_cache=True, use_oauth=True) ## https://github.com/pytube/pytube/issues/1712 (for context) for some reason there is an error with the age restriction, and the only way (that I could find till now) to bypass the age restriction error is by going to the dir where your interpretor is installed (inluding the pytube lib.), open " innertube.py " and in  line 223 change ANDROID_MUSIC to !!! ANDROID, IOS, IOS_EMBED, or ANDROID_CREATOR
         title = yt.title
         duration = yt.length
         print(f"Downloading: {title}")
@@ -42,8 +42,11 @@ def download_from_txt(file_path, output_path="."):
 
 def main():
     user_input  = input("Enter YouTube link or path to .txt document: ")
+    user_input.strip('"')
+    
     output_folder = input("Enter output folder (press Enter for the current folder): ") or "."
-
+    output_folder.strip('"')
+    
     if user_input.lower().endswith('.txt'):
         download_from_txt(user_input, output_folder)
     else:
